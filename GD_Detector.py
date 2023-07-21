6aee1b991d00b84be8a9b8ddaad32540f623a86d
TAOLI <120351660+lt11300@users.noreply.github.com>
2023-07-21 10:42:59 +0800

    Update GD_Detector.py

--- GD_Detector.py
+++ GD_Detector.py
@@ -4,7 +4,7 @@ def get_nodes(Phylo_t: object) -> list:
     nodes = [node for node in Phylo_t.traverse() if not node.is_leaf()]
     return nodes
 
-def Empty_matrix_creation(Leaves: list) -> pd.DataFrame:
+def empty_matrix_creation(Leaves: list) -> pd.DataFrame:
     df_empty = pd.DataFrame(0, columns=Leaves, index=Leaves)
     return df_empty
 
@@ -16,35 +16,35 @@ def get_empty_count_dict(Leaves: list) -> dict:
             empty_count_dic[L2 + ":" + L1] = 0
     return empty_count_dic
 
-def Batch_GFs_traverse(tre_dic: dict, support_value: int, empty_count_dic: dict,imap:dict) -> dict:
+def batch_gfs_traverse(tre_dic: dict, support_value: int, empty_count_dic: dict,imap:dict) -> dict:
     for tre_path in tre_dic.values():
         Phylo_t0 = read_tree(tre_path)
         Phylo_t1 = root_tre_with_midpoint_outgroup(Phylo_t0)
-        Nodes = get_nodes(Phylo_t1)
-        for index, Node in enumerate(Nodes):
-            Node_confidence = Node.support
+        nodes = get_nodes(Phylo_t1)
+        for index, node in enumerate(nodes):
+            node_confidence = node.support
 
-            sub_clade1, sub_clade2 = Node.children
+            sub_clade1, sub_clade2 = node.children
             
-            Leaves_sub_clade1 = sub_clade1.get_leaf_names()
-            Leaves_sub_clade2 = sub_clade2.get_leaf_names()
+            leaves_sub_clade1 = sub_clade1.get_leaf_names()
+            leaves_sub_clade2 = sub_clade2.get_leaf_names()
             
-            Taxon_sub_clade1 = {imap[leaf] for leaf in Leaves_sub_clade1}
-            Taxon_sub_clade2 = {imap[leaf] for leaf in Leaves_sub_clade2}
+            taxon_sub_clade1 = {imap[leaf] for leaf in leaves_sub_clade1}
+            taxon_sub_clade2 = {imap[leaf] for leaf in leaves_sub_clade2}
 
-            if Taxon_sub_clade1.isdisjoint(Taxon_sub_clade2):
-                if Node_confidence is not None and Node_confidence >= support_value:
-                    for L1 in Leaves_sub_clade1:
-                        for L2 in Leaves_sub_clade2:
+            if taxon_sub_clade1.isdisjoint(taxon_sub_clade2):
+                if node_confidence is not None and node_confidence >= support_value:
+                    for L1 in leaves_sub_clade1:
+                        for L2 in leaves_sub_clade2:
                             empty_count_dic[imap[L1] + ":" + imap[L2]] += 1
                             empty_count_dic[imap[L2] + ":" + imap[L1]] += 1
     return empty_count_dic
 
 def sps_pair_count_dict(sptree: object, tre_dic: dict, imap: dict) -> dict:
     num_tre_node(sptree)
-    Leaves = sptree.get_leaf_names()
-    empty_count_dic = get_empty_count_dict(Leaves)
-    df_empty = Empty_matrix_creation(Leaves)
+    leaves = sptree.get_leaf_names()
+    empty_count_dic = get_empty_count_dict(leaves)
+    df_empty = Empty_matrix_creation(leaves)
     dic_pair_count = Batch_GFs_traverse(tre_dic, 50, empty_count_dic, imap)
 
     sps_pair_count_dic = {}
@@ -63,7 +63,7 @@ def sps_pair_count_dict(sptree: object, tre_dic: dict, imap: dict) -> dict:
 
     return sps_pair_count_dic
 
-def Summary_count(dic_pair_count: dict, empty_count_dic: pd.DataFrame) -> pd.DataFrame:
+def summary_count(dic_pair_count: dict, empty_count_dic: pd.DataFrame) -> pd.DataFrame:
     for key, value in dic_pair_count.items():
         L1, L2 = key.split(":")
         df_empty.loc[L1, L2] = value
@@ -71,10 +71,10 @@ def Summary_count(dic_pair_count: dict, empty_count_dic: pd.DataFrame) -> pd.Dat
 
 def sps_pair_count_dict(sptree: object, tre_dic: dict, imap: dict) -> dict:
     num_tre_node(sptree)
-    Leaves = sptree.get_leaf_names()
-    empty_count_dic = get_empty_count_dict(Leaves)
-    df_empty = Empty_matrix_creation(Leaves)
-    dic_pair_count = Batch_GFs_traverse(tre_dic, 50, empty_count_dic, imap)
+    leaves = sptree.get_leaf_names()
+    empty_count_dic = get_empty_count_dict(leaves)
+    df_empty = empty_matrix_creation(leaves)
+    dic_pair_count = batch_gfs_traverse(tre_dic, 50, empty_count_dic, imap)
 
     sps_pair_count_dic = {}
     for node in sptree.traverse():
@@ -101,7 +101,7 @@ def mark_sptree(sptree:object,sps_pair_count_dic:dict)->object:
         nstyle["size"] = 0
         nstyle["shape"] = "circle"
         node.set_style(nstyle)
-        if not node.is_leaf() and node.name in a:
+        if not node.is_leaf() and node.name in sps_pair_count_dic:
             num=str(sps_pair_count_dic[node.name])
             node.add_face(TextFace(num+' GD', fsize=5, fgcolor="red"), column=0, position="branch-top")
             
