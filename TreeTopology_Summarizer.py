3f9bb712e7a05ccce8f607ce4f5ed39621595cc0
TAOLI <120351660+lt11300@users.noreply.github.com>
2024-01-14 04:11:47 +0800

    Update TreeTopology_Summarizer.py

--- TreeTopology_Summarizer.py
+++ TreeTopology_Summarizer.py
@@ -1,14 +1,6 @@
 from __init__ import *
 import os
 
-def get_empty_count_dict(sptree: object) -> dict:
-    empty_count_dic={}
-    for node in sptree.traverse():
-        if not node.is_leaf():
-            clade=node.write(format=9)
-            empty_count_dic[clade]=0
-    return empty_count_dic
-
 def switch_tree(phylo_tree):
     
     down_child = phylo_tree.get_children()[1]
@@ -20,53 +12,127 @@ def switch_tree(phylo_tree):
     
     return new_tree
 
+def switch_tree(phylo_tree):
+    new_tree=phylo_tree.copy()
+    new_tree.swap_children()
+    return new_tree
+    
 def get_only_sps_tree(Phylo_t):
     for node in Phylo_t:
         node.name=node.name.split('_')[0]
     return Phylo_t
-
-def folding_tree(Phylo_t):
-    for node in Phylo_t.traverse():
+    
+def get_species_counts_dic(node):
+    species_repeats = {}
+    for leaf in node:
+        species = leaf.name
+        if species in species_repeats:
+            species_repeats[species] += 1
+        else:
+            species_repeats[species] = 1
+    return species_repeats
+    
+def folding_tree(Phylo_t):  
+    Phylo_t_c=Phylo_t.copy()
+    for i in Phylo_t_c.traverse():
+        i.add_feature('label',1)
+    
+    sp_set=set()
+    for node in Phylo_t_c.traverse():
         if not node.is_leaf():
             sps=get_species_list(node)
             if len(set(sps))==1:
-                node.name=sps[0]
+                node.name=sps[0]#+'_'+str(len(sps))
+                #node.add_feature('label',str(len(sps)))
+                node.label=len(sps)
                 for child in node.get_children():
                     child.detach() 
-    return Phylo_t
-
-def statistical_clade(clade_dic,Phylo_t):
-    for node in Phylo_t.traverse():
-        if not node.is_leaf():
-            clade=node.write(format=9)
-            if clade in clade_dic :
-                clade_dic[clade]+=1
             else:
-                clade_dic[clade]=1
+                sp=tuple(get_species_set(node))
+                if sp in sp_set:
+                    continue
+                else:
+                    sp_set.add(sp)
+                    if len(node.children) == 2:
+                        left_child = node.children[0]
+                        right_child = node.children[1]
+                        node_up=node.up
+                        if has_dup_clade(node):
+                            s=get_species_counts_dic(node)
+                            s1=str(list(set(s.values()))[0])
+                            if len(left_child) >len(right_child):
+                                right_child.label=int(s1)
+                                if node_up:
+                                    node.detach()
+                                    node_up.add_child(right_child)
+                                else:
+                                    left_child.detach()
+                            else:
+                                left_child.label=int(s1)
+                                if node_up:
+                                    node.detach()
+                                    node_up.add_child(left_child)
+                                else:
+                                    right_child.detach()      
+    return Phylo_t_c
 
-def get_multiplier(t):
-    multiplier=1
-    for i in t.traverse():
-        if not i.is_leaf():
-            sps=get_species_list(i)
-            if len(set(sps))==1:
-                 multiplier=multiplier*len(sps)
-    return multiplier
-                
+def get_species_set(node):
+    return set(leaf.name for leaf in node.iter_leaves())
+    
+def has_dup_clade(tree):
+    def compare_branch(node):
+        left_child = node.children[0]
+        right_child = node.children[1]
+        if get_species_set(left_child) ==get_species_set(right_child):
+        
+            return True
+
+    return compare_branch(tree)
+
 def rejust_clade_dic(clade_dic):
     new_dic={}
     for k,v in clade_dic.items():
         t=Tree(k)
-        t1=switch_tree(t)
-        t2=t1.write(format=9)
-        if k and t2 not in new_dic :
-            new_dic[k]=v
-        if t2 in new_dic :
-            new_dic[t2]=new_dic[t2]+v
+        
+        if len(t.get_children())==2:
+            t1=switch_tree(t)
+            t2=t1.write(format=9)
+            t3=t.write(format=9)
+        else:
+            tt=t.get_children()[0]
+            t1=switch_tree(tt)
+            t2=t1.write(format=9)
+            t3=tt.write(format=9)
+            
+        if t2 in new_dic  :
+            new_dic[t3]=new_dic[t3]+v
+        else:
+            new_dic[t2]=v
+        
             
     return new_dic
 
+def get_num(node):
+    def multiply_elements(lst):
+        result = 1
+        for num in lst:
+            result *= num
+        return result
+    lst=[]
+    for i in node.traverse():
+        lst.append(i.label)
+    return multiply_elements(lst)
 
+def get_relative_clade_dic(relative_clade_dic,Phylo_t):
+    for node in Phylo_t.traverse('postorder'):
+        if not node.is_leaf():
+            clade=node.write(format=9)
+            num=get_num(node)
+            if clade in relative_clade_dic :
+                relative_clade_dic[clade]+=num
+            else:
+                relative_clade_dic[clade]=num
+                
 def statistical_main(tre_dic,gene2new_named_gene_dic,voucher2taxa_dic):
     relative_clade_dic={}
     obtain_clade_dic={}
@@ -76,12 +142,12 @@ def statistical_main(tre_dic,gene2new_named_gene_dic,voucher2taxa_dic):
         Phylo_t1=rename_input_tre(Phylo_t0,gene2new_named_gene_dic)
         Phylo_t1=get_only_sps_tree(Phylo_t0)
         Phylo_t2=folding_tree(Phylo_t1)
-        statistical_clade(relative_clade_dic,Phylo_t2)
-        statistical_clade(obtain_clade_dic,Phylo_t1)
-    relative_new_dic=rejust_clade_dic(relative_clade_dic)
-    obtain_new_dic=rejust_clade_dic(obtain_clade_dic)
-    relative_sorted_dict = dict(sorted(relative_new_dic.items(), key=lambda x: len(x[0]), reverse=True))
-    obtain_sorted_dict = dict(sorted(obtain_new_dic.items(), key=lambda x: len(x[0]), reverse=True))
+        get_relative_clade_dic(relative_clade_dic,Phylo_t2)
+        get_obtain_clade_dic(obtain_clade_dic,Phylo_t2)
+    #relative_new_dic=rejust_clade_dic(relative_clade_dic)
+    #obtain_new_dic=rejust_clade_dic(obtain_clade_dic)
+    relative_sorted_dict = dict(sorted(relative_clade_dic.items(), key=lambda x: len(x[0]), reverse=True))
+    obtain_sorted_dict = dict(sorted(obtain_clade_dic.items(), key=lambda x: len(x[0]), reverse=True))
     with open ('Relative_Statistical_calde.txt','w') as f :
         for k,v in relative_sorted_dict.items():
             t=Tree(k)
@@ -90,11 +156,8 @@ def statistical_main(tre_dic,gene2new_named_gene_dic,voucher2taxa_dic):
     with open ('Obtain_Statistical_calde.txt','w') as f :
         for k,v in obtain_sorted_dict.items():
             t=Tree(k)
-            if len(set(get_species_list(t))) !=len(t.get_leaf_names()):
-                s=get_multiplier(t)
-                folding_tree(t)
-                rename_input_tre(t,voucher2taxa_dic)
-                f.write(t.write(format=9)+'\t'+str(v*s)+'\n')
+            rename_input_tre(t,voucher2taxa_dic)
+            f.write(t.write(format=9)+'\t'+str(v)+'\n')
 
 
 if __name__ == "__main__":
