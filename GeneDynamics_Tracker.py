6fac58260bd4dab7db260ddbd49fd613a70b6f83
TAOLI <120351660+lt11300@users.noreply.github.com>
2024-02-23 12:11:20 +0800

    Update GeneDynamics_Tracker.py

--- GeneDynamics_Tracker.py
+++ GeneDynamics_Tracker.py
@@ -1,16 +1,17 @@
 import sys
 import subprocess
+import os
 
 def run_dolloparsimony(file1, file2, file3, output_file):
     command_list = ["./software/dolloparsimony" , file1, file2, file3, output_file]
     try:
         subprocess.run(command_list, check=True)
-        print("Program executed successfully！")
+        print("Program executed successfully!")
     except subprocess.CalledProcessError as e:
         print("Program execution failed:", e)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 6:
+    if len(sys.argv) != 5:
         print("Usage: python GeneDynamics_Tracker.py file1 file2 file3 output_file")
         sys.exit(1)
     
@@ -19,4 +20,6 @@ if __name__ == "__main__":
     file3 = sys.argv[3]
     output_file = sys.argv[4]
     
+
     run_dolloparsimony(file1, file2, file3, output_file)
+
