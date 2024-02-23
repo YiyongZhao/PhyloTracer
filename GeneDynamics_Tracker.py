258bbed4d0fb51946b58c80b011b29a2a1c84905
TAOLI <120351660+lt11300@users.noreply.github.com>
2024-02-23 11:01:00 +0800

    Update GeneDynamics_Tracker.py

--- GeneDynamics_Tracker.py
+++ GeneDynamics_Tracker.py
@@ -1,1 +1,15 @@
+import subprocess
+software_path = "./software/dolloparsimony"  
 
+
+mcl_file = "input.mcl"
+other_parameters = ["--param1", "value1", "--param2", "value2"]  
+
+
+command = [software_path, mcl_file] + other_parameters
+
+try:
+    subprocess.run(command, check=True)
+    print("Program executed successfully！")
+except subprocess.CalledProcessError as e:
+    print("Program execution failed!", e)
