f242fbb4c63da05288c794187d391c70d0ece672
TAOLI <120351660+lt11300@users.noreply.github.com>
2024-03-14 00:44:30 +0800

    Add files via upload

---
+++ 2_PhyloSupport_Scaler/test.sh
@@ -0,0 +1,1 @@
+python ../../Phylo_Tracer.py PhyloSupport_Scaler --input_GF_list GF.txt --scale 0
---
+++ 3_BranchLength_NumericConverter/test.sh
@@ -0,0 +1,1 @@
+ python ../../Phylo_Tracer.py BranchLength_NumericConverter --input_GF_list GF.txt --decimal_place 5
