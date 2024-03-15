01ee482e51d40807501f1557b61d879e9b3b695f
TAOLI <120351660+lt11300@users.noreply.github.com>
2024-03-15 16:07:13 +0800

    Add files via upload

--- example_date/1_PhyloTree_CollapseExpand/test.sh
+++ example_date/1_PhyloTree_CollapseExpand/test.sh
@@ -1,1 +1,1 @@
-python ../../Phylo_Tracer.py PhyloTree_CollapseExpand --input_GF_list GF.txt --support_value 70 
+python ../../Phylo_Tracer.py PhyloTree_CollapseExpand --input_GF_list GF.txt --support_value 50
