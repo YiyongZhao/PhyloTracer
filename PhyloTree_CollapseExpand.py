e39126e85e78f7b379f2041bf639afa416d1b00b
TAOLI <120351660+lt11300@users.noreply.github.com>
2024-03-15 15:48:31 +0800

    Update PhyloTree_CollapseExpand.py

--- PhyloTree_CollapseExpand.py
+++ PhyloTree_CollapseExpand.py
@@ -3,21 +3,29 @@ from __init__ import *
 from BranchLength_NumericConverter import write_tree_to_newick
 
 def collapse_expand(Phylo_t:object,support_value:int)->object:
-	for node in Phylo_t.tranverse():
+	for node in Phylo_t.traverse():
 		if not node.is_leaf():
-			if node.support >support_value:
+			if node.support <support_value:
 			     node.delete()
 
 	return Phylo_t
 
-if __name__ == "__main__":
-    dir_path1 = os.path.join(os.getcwd(), "pruned_tree")
-    if os.path.exists(dir_path1):
-        shutil.rmtree(dir_path1)
-    os.makedirs(dir_path1)
-    tre_dic=read_and_return_dict('100_nosingle_GF_list.txt')
-    support_value=50
+def collapse_expand_main(tre_dic:dict,support_value:int):
+    dir_path = os.path.join(os.getcwd(), "output/collapse_expand_tree/")
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+    os.makedirs(dir_path)
     for  k,v in tre_dic.items():
-    	t=Tree(v)
-    	t1=collapse_expand(t)
-    	write_tree_to_newick(t1,k,dir_path1)
+        t=Tree(v)
+        t1=collapse_expand(t,support_value)
+        tree_str=t1.write(format=0)
+        write_tree_to_newick(tree_str,k,dir_path)
+
+
+
+if __name__ == "__main__":
+    
+    tre_dic=read_and_return_dict('GF.txt')
+    support_value=50
+    collapse_expand_main(tre_dic,support_value,keep_sc,decimal_places)
+    
