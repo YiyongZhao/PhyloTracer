84448f1ecb268d5a6db051241d423276101824ef
TAOLI <120351660+lt11300@users.noreply.github.com>
2024-06-27 12:42:01 +0800

    Update PhyloSupport_Scaler.py

--- PhyloSupport_Scaler.py
+++ PhyloSupport_Scaler.py
@@ -1,42 +1,39 @@
-
 from __init__ import *
 from BranchLength_NumericConverter import write_tree_to_newick
 
-def scale_support(Phylo_t:object,scale:int):
-    max_support=max([i.support for i in Phylo_t.traverse()])
-    if max_support<1:
-        if scale=='100':
+def scale_support(Phylo_t: object, scale: str) -> object:
+    max_support = max(node.support for node in Phylo_t.traverse())
+    
+    if max_support < 1:
+        if scale == '100':
             for node in Phylo_t.traverse():
-                node.support=node.support*100
-            return Phylo_t
-                
+                node.support *= 100
         else:
-            return Phylo_t
-            print('there is no need to scale_down_support ,because the tree node support is all <1')
-            
-            
+            print('No need to scale down support, as all tree node supports are already < 1.')
     else:
-        if scale=='100':
-            
-            
-            return Phylo_t
-            print('there is no need to scale_support ,because the tree node support is in range of [1,100]')
-        else:
+        if scale != '100':
             for node in Phylo_t.traverse():
-                node.support=node.support/100
-            
-            return Phylo_t
+                node.support /= 100
+        else:
+            print('No need to scale support, as tree node supports are already in the range [1, 100].')
+    
+    return Phylo_t
+
 
 def support_scaler_main(tre_dic,scale):
-    dir_path = os.path.join(os.getcwd(), "output/support_scaler_tree/")
+    dir_path = os.path.join(os.getcwd(), "support_scaler_tree/")
     if os.path.exists(dir_path):
         shutil.rmtree(dir_path)
     os.makedirs(dir_path)
+    pbar = tqdm(total=len(tre_dic), desc="Processing trees", unit="tree")
     for tre_ID,tre_path in tre_dic.items():
+        pbar.set_description(f"Processing {tre_ID}")
         t=Tree(tre_path)
         t1=scale_support(t,scale)
         tree_str=t1.write(format=0)
         write_tree_to_newick(tree_str,tre_ID,dir_path)
+        pbar.update(1)
+    pbar.close()
 
 if __name__ == "__main__":
     scale=1
